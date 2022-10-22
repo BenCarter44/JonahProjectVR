77c103c8666b8f38732b0209e14ab23a6b47df8a
Benjamin Carter <59350660+BenRobotics101@users.noreply.github.com>
2022-10-22 16:08:47 -0700

    Add comments

--- Assets/Scripts/VRCustomInputManager.cs
+++ Assets/Scripts/VRCustomInputManager.cs
@@ -5,18 +5,27 @@ using UnityEngine.XR;
 using UnityEngine.Events;
 
 // https://docs.unity3d.com/Manual/xr_input.html
+/*
+ * 
+ *   Benjamin Carter - VR Custom Input Manager Class
+ * 
+ *   This class reads input from a VR controller and allows for easy access. Most of the class is mainly custom getter methods that use the respective
+ *   getRaw method The getRaw method gets the actual value using the OpenXR API. 
+ *   This class acts as a "nice" translator between the OpenXR API and use in actual scripts.
+ */
+
 
 public class VRCustomInputManager
 {
     private InputDevice leftDevice;
 	private InputDevice rightDevice;
-    private InputDevice headsetDevice;
-    public float deadZone
+    private InputDevice headsetDevice;  // store the various input devices.
+    public float deadZone  // deadzone for the joysticks.
     {
         get; set;
     }
 
-    public float batteryLevel
+    public float batteryLevel   // store headset related information
     {
         get
         {
@@ -33,7 +42,7 @@ public class VRCustomInputManager
     }
     
     private bool err;
-    public bool isError
+    public bool isError  // store if there is any read error
     {
         get
         {
@@ -42,7 +51,7 @@ public class VRCustomInputManager
      //   private set;
     }
 
-    public VRCustomInputManager()
+    public VRCustomInputManager()  // the constructor for the class. It gets the devices from the VR headset. Dumps DEBUG information.
     {
         var allDev = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevices(allDev);
@@ -61,7 +70,7 @@ public class VRCustomInputManager
 		rightDevice = rightHandDevices[0];
         headsetDevice = headDevices[0];
     }
-    private Vector2 getRawV(InputDevice dev,InputFeatureUsage<Vector2> ug)
+    private Vector2 getRawV(InputDevice dev,InputFeatureUsage<Vector2> ug) // the raw information -- the OpenXR API
     {
         Vector2 ans;
         err = !dev.TryGetFeatureValue(ug,out ans);
