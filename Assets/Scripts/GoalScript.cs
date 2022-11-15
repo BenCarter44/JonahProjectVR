8a4be212e1eacbc7f3dd13460bfd5640330c2666
Benjamin Carter <59350660+BenRobotics101@users.noreply.github.com>
2022-11-15 14:19:07 -0700

    Add a fade

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalScript : MonoBehaviour
 {
     public GameObject RobotCarsMan;
+    public GameObject theBlackScreen;
+    private float startTimer;
+    public float fadeDir;
+    private Color startColor;
+    private Color stopColor;
+    private bool startFade;
     // Start is called before the first frame update
     void Start()
     {
-
+        startColor = new Color(0f, 0f, 0f, 0f);
+        stopColor = new Color(0f, 0f, 0f, 1f);
+        startFade = true;
+        startTimer = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(startTimer + fadeDir > Time.time)
+        {
+            Image img = theBlackScreen.GetComponent<Image>();
+            Color c = Color.Lerp(startColor, stopColor, (Time.time - startTimer)  / fadeDir);
+            img.color = c;
+        }
+        
     }
     void OnTriggerEnter(Collider col)
     {
