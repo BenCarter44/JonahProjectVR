3a6f2a0cd27684528eaad5e12ff5cac603dbe79d
Benjamin Carter <59350660+BenRobotics101@users.noreply.github.com>
2022-11-29 13:24:08 -0700

    Scene3 to main - Back from Thanksgiving (#5)
    Scene 3 stuff to main
    
    Co-authored-by: JCanode1 <114193899+JCanode1@users.noreply.github.com>
    Co-authored-by: Nathan Dilla <nathcdilla@protonmail.com>

---
+++ Assets/Scripts/RobotCars.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/*
+ *  This program spawns Robot Cars on random locations of the map. The user tries to run away from the cars to reach the destination.
+ * 
+ * 
+ */
+
+public class RobotCars : MonoBehaviour
+{
+
+    public GameObject theMainPlayer;
+    private NavMeshAgent agent;
+    public float stoppingDistance;
+    public GameObject me;
+    private bool stopCars = false;
+  //  public static
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = theMainPlayer.transform.position;
+        stopCars = true;
+        Debug.Log(transform.position);
+        agent.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!stopCars && Vector3.Distance(theMainPlayer.transform.position,me.transform.position) > stoppingDistance)
+        {
+            agent.destination = theMainPlayer.transform.position;
+            agent.enabled = true;
+           // agent.isStopped = false;
+        }
+        else
+        {
+            //agent.isStopped = true;
+            agent.enabled = false;
+            
+        }
+       
+    }
+    public void StopCars()
+    {
+        Debug.Log("Stopped!");
+        stopCars = true;
+    }
+    public void StartCars()
+    {
+        Debug.Log("Start!");
+        stopCars = false;
+    }
+    /*
+    public void OnSelect()
+    {
+        agent.enabled = false;
+        agent.isStopped = true;
+        driver = true;
+    } */
+}
