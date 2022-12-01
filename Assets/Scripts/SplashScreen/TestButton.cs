48be5463948a2d1386a03ee20d262f65a0aeeca9
JCanode1 <114193899+JCanode1@users.noreply.github.com>
2022-12-01 15:08:27 -0700

    worked on splash screen

---
+++ Assets/Scripts/SplashScreen/TestButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+
+
+public class TestButton : MonoBehaviour
+{
+    public  void Scene1() {  
+        SceneManager.LoadScene("Scene0"); 
+        }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
