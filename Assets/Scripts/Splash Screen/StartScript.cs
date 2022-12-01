fa54f07a17249bdb8530b3df681ac535c229bccc
JCanode1 <114193899+JCanode1@users.noreply.github.com>
2022-12-01 16:16:03 -0700

    splash screen stuff
    add scripts for changing to each screen made the first screen

--- Assets/Scripts/Splash Screen/StartScript.cs
+++ Assets/Scripts/Splash Screen/StartScript.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartButton : MonoBehaviour
+
+public class StartScript : MonoBehaviour
 {
+    public void StartSceneChanger()
+    {
+        SceneManager.LoadScene("Scene0");
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
