bd4c818ade372f6e58e54ff571a6d21b3a54b21d
Benjamin Carter <59350660+BenRobotics101@users.noreply.github.com>
2022-12-05 22:32:12 -0700

    Adding VR to scene0 (#12)
    * Implemented VR
    
    * Initial VR imp
    
    * New Sea
    
    * Fix Water Poly
    
    * Save
    
    * Before double XR removal
    
    * Fixing Scene 0 to VR
    
    * WORKING SCENE 0!!! VR ENABLED

--- Assets/Scripts/Story.cs
+++ Assets/Scripts/Story.cs
@@ -3,104 +3,246 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using TMPro;
 
 
 public class Story : MonoBehaviour
 {
-    Ray ray;
-    RaycastHit hit;
+
     public Canvas canvas;
     public DialogueService ds;
-    public Text dialogueTextBox;
-    public Text actionTextBox;
-
+    public TextMeshProUGUI dialogueTextBox;
+    public TextMeshProUGUI actionTextBox;
     private Dialogue currentTalk;
 
     public TextAsset SpeechLines;
 
     public GameObject cutscene;
-
+    private VRCustomInputManager vr;
     private bool isActive = true;
-
+    private bool hitName;
+    private List<List<string>> speechAll;
+    private bool isTalking;
+    private bool aaronGo = false;
+    private bool ready = false;
     // Start is called before the first frame update
     void Start()
     {
-        var content = SpeechLines.text;
-        var AllWords = content.Split("\n");
-        List<string> speechList = new List<string>(AllWords);
+        dialogueTextBox.text = "";
+        actionTextBox.text = "";
+    //    var content = SpeechLines.text;
+     //   Debug.Log("Item");
+    //    Debug.Log(content.Split("\n"));
+    //    var AllWords = content.Split("\n");
+     //   List<string> speechList = new List<string>(AllWords);
+        
+        refreshText();
+     
 
-        ds = new DialogueService();
-
-        for(int i = 0; i <= speechList.Count-1; i++)
+     //   Invoke("testForce", 5f);
+    //    cutscene.GetComponent<StartRain>().startRain();
+       aaronGo = false;
+        vr = new VRCustomInputManager();
+        if (vr == null)
         {
-            if (speechList[i].Contains("[actor]"))
-            {
-                currentTalk = new Dialogue(speechList[i+1]);
-                
-                i=i+2;
-
-                while(speechList[i] != "[end]")
-                {
-                    currentTalk.AddSpeech(speechList[i]);
-                    i++;
-                }
-                ds.LoadDialogue(currentTalk);
-            }
+            Debug.Log("No VR Headset!");
         }
 
     //     cutscene.GetComponent<StartRain>().startRain();
     }
-
+    void testForce()
+    {
+        isActive = false;
+        cutscene.GetComponent<Overboard>().switchCam();
+    }
     // Update is called once per frame
     void Update()
     {
+     //   Debug.Log(transform.position);   
         if (isActive)
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, out hit))
+        {
+            if(vr != null && vr.buttonY && !vr.isError && ready)
             {
-                if(hit.collider.tag == "approachable")
+                Debug.Log("A: " + hitName);
+                
+                Debug.Log("B: " + hitName);
+                if(!isTalking)
                 {
-                    actionTextBox.GetComponent<Text>().text = "Press F to Talk";
-
-                    if(Input.GetKeyDown("f"))
+                    Debug.Log("C: " + hitName);
+                    isTalking = true;
+                    Invoke("nextText",0.1f);
+                    
+                }
+                
+                
+                /*
+                if(ds.isTalking == true)
+                {
+                    if (currentTalk.getIsFinished() == true)
                     {
-                        if(ds.isTalking == true)
+                        dialogueTextBox.text = "";
+                        //HARD CODED - THIS CAN BE BETTER
+                        if (currentTalk.getDialogueId() == "Aaron")
                         {
-                            if (currentTalk.getIsFinished() == true)
-                            {
-                                dialogueTextBox.GetComponent<Text>().text = "";
-                                //HARD CODED - THIS CAN BE BETTER
-                                if (currentTalk.getDialogueId() == "Aaron")
-                                {
-                                    
-                                    cutscene.GetComponent<StartRain>().startRain();
-                                }
-                                if (currentTalk.getDialogueId() == "Bach")
-                                {
-                                    isActive = false;
-                                    cutscene.GetComponent<Overboard>().switchCam();
-                                }
-                                ds.isTalking = false;
-                            }
-                            dialogueTextBox.GetComponent<Text>().text = currentTalk.talk();
+                            
+                            cutscene.GetComponent<StartRain>().startRain();
                         }
-                        else
+                        if (currentTalk.getDialogueId() == "Bach")
                         {
-                            currentTalk = ds.GetDialogue(hit.collider.name);
-                            ds.isTalking = true;
-                            dialogueTextBox.GetComponent<Text>().text = currentTalk.talk();
+                            isActive = false;
+                            cutscene.GetComponent<Overboard>().switchCam();
                         }
-                        
-                        print(hit.collider.name);
+                        ds.isTalking = false;
                     }
+                    dialogueTextBox.text = currentTalk.talk();  
                 }
                 else
                 {
-                    actionTextBox.GetComponent<Text>().text = " ";
+                    Debug.Log("HITNAME '" + hitName + "'");
+                    currentTalk = ds.GetDialogue(hitName);
+                    ds.isTalking = true;
+                    dialogueTextBox.text = currentTalk.talk();
                 }
+                */
+            } 
+        }
+        else
+        {
+            isTalking = false;
+            dialogueTextBox.text = "";
+        }
+    
+    }
+    public void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.transform.name);
+        
+        if(col.transform.name == "Aaron" || col.transform.name == "Bach")
+        {
+            GetComponent<AudioSource>().Play();
+            hitName = col.transform.name != "Aaron";
+            
+            if(col.transform.name == "Aaron")
+            {
+                ready = true;
             }
+            else if(aaronGo)
+            {
+                ready = true;
+            }
+            Debug.Log("Aaron Enter!");
+            if(isActive)
+            {
+                actionTextBox.text = "Press Y to Talk";
+            }
+        }
+    }  
+    public void OnTriggerExit(Collider col)
+    {
+     //   GetComponent<AudioSource>().Play();
+        Debug.Log(col.transform.name);
+        
+        if(col.transform.name == "Aaron" || col.transform.name == "Bach")
+        {
+            refreshText();
+            isTalking = false;
+            ready = false;
+            hitName = col.transform.name != "Aaron";
+            
+            Debug.Log("Aaron Exit!");
+            if(isActive)
+            {
+                actionTextBox.text = " ";
+            }
+        }
+    } 
+    public void nextText()
+    {
+        if(!isActive)
+        {
+             isTalking = false;
+        }
+        else
+        {
+            if(isTalking)
+            {
+                actionTextBox.text = " ";
+                dialogueTextBox.text = "";
+                int apple = 0;
+                if(hitName)
+                {
+                    apple = 1;
+                }
+                Debug.Log("D: " + apple);
+                List<string> items = speechAll[apple];
+                if(items.Count != 0)
+                {
+                    dialogueTextBox.text = items[0];
+                    Invoke("nextText",5f);
+                    items.RemoveAt(0);
+                }
+                else
+                {
+                    if (!hitName)
+                    {
+                        isTalking = false;
+                        aaronGo = true;
+                        cutscene.GetComponent<StartRain>().startRain();
+                    }
+                    else if (hitName)
+                    {
+                        isActive = false;
+                        cutscene.GetComponent<Overboard>().switchCam();
+                    }
+                }
+
+            }
+            else
+            {
+                dialogueTextBox.text = "";
+            }
+        }
+    }
+    public void refreshText()
+    {
+        var content = SpeechLines.text;
+        var AllWords = content.Split("\n");
+        List<string> speechList = new List<string>(AllWords);
+        speechAll = new List<List<string>>();
+    //    ds = new DialogueService();
+
+        string myName = "";
+        for(int i = 0; i <= speechList.Count-1; i++)
+        {
+            
+            if (speechList[i].Contains("[actor]"))
+            {
+                List<string> mySpeech = new List<string>();
+                Debug.Log("NEW CURRENT TALK");
+              //  Debug.Log(speechList[i+1]);
+         //       currentTalk = new Dialogue(speechList[i+1]);
+                myName = speechList[i+1]; 
+                i=i+2;
+
+                while(!speechList[i].Contains("[end]"))
+                {
+                    Debug.Log(speechList[i]);
+                    mySpeech.Add(speechList[i]);      
+     //               currentTalk.AddSpeech(speechList[i]);
+                    i++;
+                }
+                
+           //     Debug.Log("KEY: " + myName);
+                
+                speechAll.Add(mySpeech);
+      //          ds.LoadDialogue(currentTalk);
+            }
+        }
     }
 }
+
+
+
 
 public class DialogueService
 {
@@ -141,7 +283,7 @@ public class Dialogue
     private Action task;
     public Dialogue(string id)
     {
-        Debug.Log(id + " has been created");
+        Debug.Log("Piizzzza " + id);
         dialogueid = id;
     }
     public void AddSpeech(string text)
@@ -185,4 +327,5 @@ public class Dialogue
     {
         task();
     }
+    
 }
